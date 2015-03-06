f395fc73921ac3199047958578425fe54ddac946
Taylor Huston <taylor.a.huston@gmail.com>
2015-03-05 22:39:01 -0700

    Add collision detection

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var gridSize = 101; //Helper variable for the seize of movement squares
 
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function () {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -11,38 +11,38 @@ var Enemy = function() {
     this.x = 0;
     this.y = gridSize;
     this.speed = 1;
-    
+
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += gridSize*dt;
-    if (this.x > gridSize*6) {
+    this.x += gridSize * dt;
+    if (this.x > gridSize * 6) {
         this.x = 0;
     }
-    
+
 }
 
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+var Player = function () {
     this.sprite = 'images/char-boy.png';
     this.x = gridSize * 2;
     this.y = gridSize * 4;
 }
 
-Player.prototype.update = function(e) {
+Player.prototype.update = function (e) {
     if (e == "left") {
         this.x -= gridSize;
     };
@@ -55,24 +55,31 @@ Player.prototype.update = function(e) {
     if (e == "down") {
         this.y += gridSize;
     };
-    this.checkCollision();
-    if(this.y < gridSize) {
-        console.log("you win!");
+    this.checkCollisions();
+    if (this.y < gridSize) {
+        console.log("You Win!");
     }
 }
 
-Player.prototype.checkCollision = function () {
 
+Player.prototype.checkCollisions = function () {
+    if (allEnemies[0].y == this.y) {
+        if (allEnemies[0].x + gridSize >= player.x && allEnemies[0].x - gridSize <= player.x) {
+            console.log("collision");
+        }
+    }
 }
 
-Player.prototype.render = function() {
+
+
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-Player.prototype.handleInput = function(e) {
+Player.prototype.handleInput = function (e) {
     console.log(e);
     this.update(e);
-    
+
 }
 
 // Now instantiate your objects.
@@ -87,7 +94,7 @@ allEnemies.push(new Enemy());
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
