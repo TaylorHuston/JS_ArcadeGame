5dc06c7bcaa4e7fa5aac07f90b1fdd4b3d55c94c
Taylor Huston <taylor.a.huston@gmail.com>
2015-03-05 21:15:09 -0700

    Add rendering of all objects and basic movement

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var gridSize = 101; //Helper variable for the seize of movement squares
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,6 +8,10 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = gridSize;
+    this.speed = 1;
+    
 }
 
 // Update the enemy's position, required method for game
@@ -14,7 +20,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += gridSize*dt;
+    if (this.x > gridSize*6) {
+        this.x = 0;
+    }
+    
 }
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -24,11 +36,49 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = gridSize * 2;
+    this.y = gridSize * 4;
+}
 
+Player.prototype.update = function(e) {
+    if (e == "left") {
+        this.x -= gridSize;
+    };
+    if (e == "up") {
+        this.y -= gridSize;
+    };
+    if (e == "right") {
+        this.x += gridSize;
+    };
+    if (e == "down") {
+        this.y += gridSize;
+    };
+    this.checkCollision();
+}
+
+Player.prototype.checkCollision = function () {
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(e) {
+    console.log(e);
+    this.update(e);
+    
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var player = new Player();
+var allEnemies = new Array();
+allEnemies.push(new Enemy());
+
 
 
 
